c59ffb1af8089cf8556cdb8d23c93c812da7682d
Shiranuit <yannick.combes@epitech.eu>
2018-10-01 15:26:36 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-#!usr/#!/usr/bin/env bash
+#!/usr/bin/env bash
 sudo apt-get install python3-pip
 pip3 install termcolor
-git clone https://github.com/Shiranuit/project_setup.git ~/project_setup
+git clone https://github.com/Shiranuit/project_setup_epitech.git ~/project_setup
+sudo rm /usr/bin/project
 sudo cp ~/project_setup/project.py /usr/bin/project
+sudo rm ~/project_setup -r -f
